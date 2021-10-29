a6ac1d1013bc16d7a057f10a30a53b41f20a04af
stanleyszysz <carracass@gmail.com>
2021-10-29 20:19:40 +0200

    OOP1 - Task2

---
+++ oop1/src/main/java/fc/introduction/movie/Movie.java
@@ -0,0 +1,11 @@
+package fc.introduction.movie;
+
+public class Movie {
+    public static void main(String[] args) {
+        MovieFeatures movieFeatures = new MovieFeatures("Rejs", "Marek Piwowski", 66, 1970);
+
+        movieFeatures.aboutMovie();
+    }
+
+
+}
