e1ff87b46ac37f4e4d02466d7b5b9cbec5f7ac81
stanleyszysz <carracass@gmail.com>
2021-11-06 23:44:31 +0100

    OOP-2 Task-7 Days of the week

---
+++ oop2/src/main/java/fc/introduction/days/Days.java
@@ -0,0 +1,9 @@
+package fc.introduction.days;
+
+public class Days {
+    public static void main(String[] args) {
+        DayName dayName = DayName.SATURDAY;
+        System.out.println(dayName);
+    }
+
+}
