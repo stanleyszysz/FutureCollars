155558d260bc7a2ff6e6ef69f975c17775679d7e
stanleyszysz <carracass@gmail.com>
2021-11-06 09:15:39 +0100

    OOP-2 Task-1 Message class corrections

--- oop2/src/main/java/fc/introduction/message/ConsoleMessage.java
+++ oop2/src/main/java/fc/introduction/message/ConsoleMessage.java
@@ -5,7 +5,6 @@ public class ConsoleMessage extends Message{
     @Override
     String getMessage() {
         String className = this.getClass().getSimpleName();
-        System.out.println("This is class " + className);
         return className;
     }
 }
--- oop2/src/main/java/fc/introduction/message/WindowMessage.java
+++ oop2/src/main/java/fc/introduction/message/WindowMessage.java
@@ -5,7 +5,6 @@ public class WindowMessage extends Message{
     @Override
     String getMessage() {
         String className = this.getClass().getSimpleName();
-        System.out.println("This is class " + className);
         return className;
     }
 }
