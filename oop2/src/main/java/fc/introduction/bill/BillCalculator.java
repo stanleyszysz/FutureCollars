ad79b39ee4ca370c69330e9f6f968320f6a04d16
stanleyszysz <carracass@gmail.com>
2021-11-07 15:36:28 +0100

    OOP-2 Task-6 Bill corrections

--- oop2/src/main/java/fc/introduction/bill/BillCalculator.java
+++ oop2/src/main/java/fc/introduction/bill/BillCalculator.java
@@ -1,24 +1,17 @@
 package fc.introduction.bill;
 
 public class BillCalculator {
-    double billValue;
-    float servicePayment;
-    double discount;
-    short takeawayPackagingFee;
 
     double calculate(double billValue, float servicePayment) {
-        double totalAmount = billValue + billValue * servicePayment;
-        return totalAmount;
+        return billValue + billValue * servicePayment;
     }
 
     double calculate(double billValue, float servicePayment, double discount) {
         double billValueAfterDiscount = (1 - discount) * billValue;
-        double totalAmount = billValueAfterDiscount + billValueAfterDiscount * servicePayment;
-        return totalAmount;
+        return billValueAfterDiscount + billValueAfterDiscount * servicePayment;
     }
 
     double calculate(double billValue, float servicePayment, short takeawayPackagingFee) {
-        double totalAmount = billValue + billValue * servicePayment + takeawayPackagingFee;
-        return totalAmount;
+        return billValue + billValue * servicePayment + takeawayPackagingFee;
     }
 }
