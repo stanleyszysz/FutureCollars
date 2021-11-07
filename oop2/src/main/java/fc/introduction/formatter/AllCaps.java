61b579eb6352e6b0ac306e600b7104013a46555d
stanleyszysz <carracass@gmail.com>
2021-11-07 15:31:02 +0100

    OOP-2 Task-3 Text Formatter corrections

--- oop2/src/main/java/fc/introduction/formatter/AllCaps.java
+++ oop2/src/main/java/fc/introduction/formatter/AllCaps.java
@@ -1,11 +1,9 @@
 package fc.introduction.formatter;
 
 public class AllCaps implements TextFormatter{
-    String text;
 
     @Override
     public String formatText(String text) {
-        String changedText = text.toUpperCase();
-        return changedText;
+        return text.toUpperCase();
     }
 }
--- oop2/src/main/java/fc/introduction/formatter/SmallCaps.java
+++ oop2/src/main/java/fc/introduction/formatter/SmallCaps.java
@@ -1,11 +1,9 @@
 package fc.introduction.formatter;
 
 public class SmallCaps implements TextFormatter{
-    String text;
 
     @Override
     public String formatText(String text) {
-        String changedText = text.toLowerCase();
-        return changedText;
+        return text.toLowerCase();
     }
 }
