12e09e0a13a51fa7b949cbaaca5fa33b04dfc815
stanleyszysz <carracass@gmail.com>
2021-11-09 21:24:52 +0100

    CC Task-2 FizzBuzz

---
+++ cleancode/src/main/java/future/collars/fizzbuzz/Main.java
@@ -0,0 +1,8 @@
+package future.collars.fizzbuzz;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz(1, 100);
+        fizzBuzz.calculateIfFizzBuzz();
+    }
+}
