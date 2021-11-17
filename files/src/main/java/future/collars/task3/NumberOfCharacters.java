6bc9cefcfdfe20f57fe78794a16c3b8f582f2f6c
stanleyszysz <carracass@gmail.com>
2021-11-17 21:06:38 +0100

    Files Task-3

---
+++ files/src/main/java/future/collars/task3/NumberOfCharacters.java
@@ -0,0 +1,16 @@
+package future.collars.task3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class NumberOfCharacters {
+
+    private static final String PATH_TO_RESOURCES = "src/main/resources/";
+
+    public static void calculateNumberOfCharacters(String nameOfFile) throws IOException {
+
+        String text = Files.readString(Path.of(PATH_TO_RESOURCES + nameOfFile));
+        System.out.println(text.length());
+    }
+}
