3c72aa3dbe48bf6a17e0de9954161c3a324476c5
stanleyszysz <carracass@gmail.com>
2021-11-17 19:54:14 +0100

    Files Task-0 corrections

--- files/src/main/java/future/collars/task0/FileWriter.java
+++ files/src/main/java/future/collars/task0/FileWriter.java
@@ -7,9 +7,11 @@ import java.nio.file.Path;
 
 class FileWriter {
 
+    private static final String PATH_TO_RESOURCES = "src/main/resources/";
+
     public static void writeToFile(String nameOfFile, String textToWrite) throws IOException {
 
-        Files.write(Path.of("src/main/resources/" + nameOfFile), textToWrite.getBytes(StandardCharsets.UTF_8));
+        Files.write(Path.of(PATH_TO_RESOURCES + nameOfFile), textToWrite.getBytes(StandardCharsets.UTF_8));
     }
 
 }
