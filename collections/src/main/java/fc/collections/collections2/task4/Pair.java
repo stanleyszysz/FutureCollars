6421d85a09df093ff8e3ab5521c4996a3575421c
stanleyszysz <carracass@gmail.com>
2021-12-04 19:26:10 +0100

    Collections-2 Task-4

---
+++ collections/src/main/java/fc/collections/collections2/task4/Pair.java
@@ -0,0 +1,19 @@
+package fc.collections.collections2.task4;
+
+public class Pair<K, V> {
+    private final K first;
+    private final V second;
+
+    public Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+}
