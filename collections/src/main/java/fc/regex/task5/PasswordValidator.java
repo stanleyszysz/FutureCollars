11c2b1ed997fb06256a215612a365134ef308d01
stanleyszysz <carracass@gmail.com>
2021-12-09 16:11:30 +0100

    Optional Task-5 corrections

--- collections/src/main/java/fc/regex/task5/PasswordValidator.java
+++ collections/src/main/java/fc/regex/task5/PasswordValidator.java
@@ -4,8 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PasswordValidator {
+
+    private static final Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?!.*?(?:2021|21)).{7,}$");
+
     public static boolean checkPassword(String password) {
-        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[A-Z])(?!.*?(?:2021|21)).{7,}$");
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
